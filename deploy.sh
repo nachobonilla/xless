c514a490c9125798050c66419c2083654ea33426
nachobonilla <nachoo.b@hotmail.com>
2019-08-29 17:24:39 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 read -p "Enter Slack Incoming Hook: " "slack_incoming_webhook"
-now secret add slack-incoming-webhook "$slack_incoming_webhook"
+now secret add slack-incoming-webhook "3j4kj3kj43kj34j4kj3"
 now deploy
